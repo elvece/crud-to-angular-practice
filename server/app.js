bf93453dadaf9dcb987d0ce82a5d0526bfb1f3ed
elvece <lvcifferello@gmail.com>
2015-09-24 13:34:51 -0600

    compliled layout.html and index.html to remove swig, commented out layout and error html files, updated app.js to remove swig and set new index route

--- server/app.js
+++ server/app.js
@@ -7,10 +7,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var swig = require('swig');
+// var swig = require('swig');
 
 // *** routes *** //
-var routes = require('./routes/index.js');
+// var routes = require('./routes/index.js');
 var apiRoutes = require('./routes/api.js');
 
 
@@ -19,9 +19,9 @@ var app = express();
 
 
 // *** view engine *** //
-var swig = new swig.Swig();
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
+// var swig = new swig.Swig();
+// app.engine('html', swig.renderFile);
+// app.set('view engine', 'html');
 
 
 // *** static directory *** //
@@ -37,8 +37,12 @@ app.use(express.static(path.join(__dirname, '../client/public')));
 
 
 // *** main routes *** //
-app.use('/', routes);
+// app.use('/', routes);
 app.use('/api/v1/', apiRoutes);
+
+app.use('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 
 // catch 404 and forward to error handler
