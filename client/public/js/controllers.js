34336fec34216fd183a94067c625efce2dce59b8
elvece <lvcifferello@gmail.com>
2015-09-24 14:48:41 -0600

    added post request, deleted now unecessary files

--- client/public/js/controllers.js
+++ client/public/js/controllers.js
@@ -1,7 +1,44 @@
-app.controller('beerController', function($scope, httpFactory){
+app.controller('beerController', function($scope, httpFactory, $timeout){
+
+  $scope.success = false;
+
+  //sometimes need to clear form?
+  // $scope.beer = {};
+
+  //original http get before adding factory
   // $http.get('/api/v1/beers')
   // .then(function(response){
   //   $scope.beers = response.data;
   // });
+
+  getBeers = function(url){
+
+    httpFactory.get(url)
+    .then(function(response){
+      $scope.beers = response.data;
+    });
+  };
+
+  getBeers('/api/v1/beers');
+
+  function messageTimeout(){
+    $scope.success = false;
+  }
+
+  $scope.postBeer = function(){
+
+    var payload = $scope.beer;
+
+    httpFactory.post('/api/v1/beers', payload)
+    .then(function(response){
+      $scope.beers.push(response.data);
+      $scope.beer = {};
+      $scope.success = true;
+      $scope.message = "Added a new beer. Thanks!";
+      $timeout(messageTimeout, 5000);
+    });
+  };
+
+
 
 });
--- client/public/js/services.js
+++ client/public/js/services.js
@@ -3,9 +3,12 @@ app.factory('httpFactory', ['$http', function($http){
   var obj = {};
 
   //get request
-  obj.get = function(){
-    return $http.get('/api/v1/beers');
+  obj.get = function(url){
+    return $http.get(url);
+  };
 
+  obj.post = function(url, payload){
+    return $http.post(url, payload);
   };
 
   return obj;
